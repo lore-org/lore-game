3fcf4ce32d463bd91017bc74724368e022f976cd
Lexi <williambush357@gmail.com>
2025-07-16 10:21:26 -0400

    use try_compile to see if -fexperimental-library is supported + use multiple jobs for cmake

--- cmake/par_exec_support_test.cpp
+++ cmake/par_exec_support_test.cpp
@@ -1,7 +1,7 @@
 #include <execution>
 #include <vector>
 
-void func() {
+void func(void) {
     std::vector<int> v = {};
     std::sort(std::execution::par, v.begin(), v.end());
     std::sort(std::execution::par_unseq, v.begin(), v.end());
